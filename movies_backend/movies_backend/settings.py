484eb2bfcf2aa7d9533b9b986844af7f9a1ead82
momo <monimsrw@outlook.fr>
2025-07-28 02:47:11 +0100

    Setting up a PostgreSQL database & Configuring  Django to use PostgreSQL.

--- movies_backend/movies_backend/settings.py
+++ movies_backend/movies_backend/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +25,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2pi%twuw^_%16l8r^9ky+!2htqj=0#)&2xzel208gh55p^kq!i'
-
+# SECRET_KEY = 'django-insecure-2pi%twuw^_%16l8r^9ky+!2htqj=0#)&2xzel208gh55p^kq!i'
+SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = []
 
@@ -74,8 +80,12 @@ WSGI_APPLICATION = 'movies_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
